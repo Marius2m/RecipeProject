d83ee39385da7f93f77835c247a85596995cc4b0
Marius Mircea <mariusmircea@outlook.com>
2019-04-06 12:00:45 +0300

    Added basic navigation functionality
    User can navigate between Recipe page and Shopping List page.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'RecipeProject';
+  currentTab = 'recipe';
+
+  onNavigate(tabSelected: string){
+    this.currentTab = tabSelected;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import {Component} from "@angular/core";
+import {Component, ElementRef, EventEmitter, Output, ViewChild} from "@angular/core";
 
 @Component({
   selector: 'app-header',
@@ -6,5 +6,19 @@ import {Component} from "@angular/core";
 })
 
 export class HeaderComponent{
+  @Output() tabClicked = new EventEmitter<string>();
+
+  @ViewChild('recipeOption') recipeIsClicked: ElementRef;
+
+
+  onRecipeClicked(){
+    this.tabClicked.emit('recipe');
+    console.log("recipe");
+  }
+
+  onShoppingClicked(){
+    this.tabClicked.emit('shopping-list');
+    console.log("shop");
+  }
 
 }
