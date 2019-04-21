ea189e48fb9c235b1438664e992dae4bb488109c
Marius Mircea <mariusmircea@outlook.com>
2019-04-21 19:01:16 +0300

    Template Driven Forms
    Shopping list now supports adding new ingredients, updating existing ingredients and deleting existing ingredients

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,26 +1,65 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {Ingredient} from "../../shared/Ingredient.model";
 import {ShoppingListService} from "../shopping-list.service";
+import {NgForm} from "@angular/forms";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent implements OnInit {
-  @ViewChild('nameInput') nameInput: ElementRef;
-
+export class ShoppingEditComponent implements OnInit, OnDestroy {
+  @ViewChild('f') shoppingListForm: NgForm;
+  amount = '';
+  subscription: Subscription;
+  editMode = false;
+  editedItemIndex: number;
+  editedItem: Ingredient;
   constructor(private shoppinglistService: ShoppingListService) { }
 
   ngOnInit() {
+    this.subscription = this.shoppinglistService.startedEditing
+      .subscribe(
+        (index: number) => {
+          this.editMode = true;
+          this.editedItemIndex = index;
+          this.editedItem = this.shoppinglistService.getIngredient(+this.editedItemIndex);
+
+          this.shoppingListForm.setValue({
+            name: this.editedItem.name,
+            amount: this.editedItem.amount,
+          });
+        }
+      );
   }
 
-  onAddClicked(amountInput: HTMLInputElement){
-    const ingredientName = this.nameInput.nativeElement.value;
-    const ingredientAmount = parseInt(amountInput.value);
-    const newIngredient = new Ingredient(ingredientName, ingredientAmount);
-    console.log("name: " + ingredientName);
-    console.log("amount: " + ingredientAmount.toString());
-    this.shoppinglistService.addIngredient(newIngredient);
+  onSubmit(form: NgForm) {
+    const value = form.value;
+    const newIngredient = new Ingredient(value.name, +this.amount);
+    console.log("name: " + value.name);
+    console.log("amount: " + this.amount);
+    if (this.editMode) {
+      this.shoppinglistService.updateIngredient(this.editedItemIndex, newIngredient);
+    } else {
+      this.shoppinglistService.addIngredient(newIngredient);
+    }
+    this.editMode = false;
+    form.reset();
   }
+
+  onClear(){
+    this.shoppingListForm.reset();
+    this.editMode = false;
+  }
+
+  onDelete() {
+    this.shoppinglistService.deleteIngredient(this.editedItemIndex);
+    this.onClear();
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -4,6 +4,7 @@ import {Subject} from "rxjs";
 
 export class ShoppingListService implements OnInit{
   ingredientsChanged = new Subject<Ingredient[]>();
+  startedEditing = new Subject<number>();
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient("Berry", 2)
@@ -18,6 +19,10 @@ export class ShoppingListService implements OnInit{
     return this.ingredients.slice();
   }
 
+  getIngredient(index: number) {
+    return this.ingredients[index];
+  }
+
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
     this.ingredientsChanged.next(this.ingredients.slice());
@@ -25,6 +30,16 @@ export class ShoppingListService implements OnInit{
 
   addIngredients(ingredients: Ingredient[]){
     this.ingredients.push(...ingredients);
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
+
+  updateIngredient(index: number, newIngredient: Ingredient) {
+    this.ingredients[index] = newIngredient;
+    this.ingredientsChanged.next((this.ingredients.slice()));
+  }
+
+  deleteIngredient(index: number) {
+    this.ingredients.splice(index,1);
     this.ingredientsChanged.next(this.ingredients.slice());
   }
 
